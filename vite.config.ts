f8f26e990914ff5943f1438ed3941f9ba2ffb290
nikeedev <nikita.gon.dev@gmail.com>
2026-07-26 13:51:55 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,35 @@
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { mdsvex } from 'mdsvex';
 
 export default defineConfig({
 	plugins: [
 		sveltekit({
 			compilerOptions: {
-				// Force runes mode for the project, except for libraries.
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			}
+			},
+
+			adapter: adapter({
+				pages: 'build',
+				assets: 'build',
+				fallback: '404.html',
+				precompress: false,
+				strict: true
+			}),
+
+			paths: {
+				base: '/blog'
+			},
+
+			preprocess: [
+				mdsvex({
+					extensions: ['.svx', '.md']
+				})
+			],
+
+			extensions: ['.svelte', '.svx', '.md']
 		})
 	]
 });
